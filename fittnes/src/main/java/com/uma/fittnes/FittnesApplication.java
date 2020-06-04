f5774f5c8867ad8f9b34466a42579812e4a31926
Fran <frdeosesfuentelu@gmail.com>
2020-06-04 19:20:17 +0200

    First commit

---
+++ fittnes/src/main/java/com/uma/fittnes/FittnesApplication.java
@@ -0,0 +1,13 @@
+package com.uma.fittnes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittnesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittnesApplication.class, args);
+	}
+
+}
---
+++ fittnes/src/test/java/com/uma/fittnes/FittnesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uma.fittnes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittnesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
